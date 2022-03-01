d1b4a4f8057ffbac791f2eb2e06fb8be1911b2c1
sahri <sarepzpt10@gmail.com>
2022-03-01 16:04:18 +0545

    initial commit

---
+++ resources/views/students/edit.blade.php
@@ -0,0 +1,71 @@
+
+@extends('students.layout')
+@section('content')
+ 
+<div class="card">
+  <div class="card-header">Contactus Page</div>
+  <div class="card-body">
+      
+      <form action="{{ url('student/' .$students->id) }}" method="post">
+        {!! csrf_field() !!}
+        @method("PATCH")
+        <input type="hidden" name="id" id="id" value="{{$students->id}}" id="id" />
+        <label>Name</label></br>
+        <input type="text" name="name" id="name" value="{{$students->name}}" class="form-control"></br>
+        <label>Address</label></br>
+        <input type="text" name="address" id="address" value="{{$students->address}}" class="form-control"></br>
+        <label>Mobile</label></br>
+        <input type="text" name="mobile" id="mobile" value="{{$students->mobile}}" class="form-control"></br>
+        <input type="submit" value="Update" class="btn btn-success"></br>
+    </form>
+   
+  </div>
+</div>
+ 
+@stop
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+@extends('students.layout')
+@section('content')
+<div class="card">
+  <div class="card-header">Contactus Page</div>
+  <div class="card-body">
+      
+      <form action="{{ url('student/' .$students->id) }}" method="post">
+        {!! csrf_field() !!}
+        @method("PATCH")
+        <input type="hidden" name="id" id="id" value="{{$students->id}}" id="id" />
+        <label>Name</label></br>
+        <input type="text" name="name" id="name" value="{{$students->name}}" class="form-control"></br>
+        <label>Address</label></br>
+        <input type="text" name="address" id="address" value="{{$students->address}}" class="form-control"></br>
+        <label>Mobile</label></br>
+        <input type="text" name="mobile" id="mobile" value="{{$students->mobile}}" class="form-control"></br>
+        <input type="submit" value="Update" class="btn btn-success"></br>
+    </form>
+  
+  </div>
+</div>
+@stop
